04327b82316b5ad535299f0e55021e3a3c6bc68d
garrettroell <garrettroell@gmail.com>
2022-05-18 13:02:49 -0500

    Added image to readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-A project that uses GitHub Actions to run everyday, and on selected days will make a git commit to draw the space invaders on the contribution map
-Today's date is: Wed May 18 15:40:44 UTC 2022
+A project that uses GitHub Actions to run everyday, and on selected days will make a git commit to draw the space invaders on the contribution map.
+
+![excel space invaders pattern](pattern.png)
--- updateLog.sh
+++ updateLog.sh
@@ -3,6 +3,7 @@ echo "Checking if a commit is needed today"
 
 # read in space invaders pattern
 PATTERN=$(head -n 1 pattern.txt)
+echo $PATTERN
 
 # calculate days since May 15 2022
 MAY_15_2022=1652572800
@@ -10,21 +11,22 @@ RUN_TIME=$(date +%s)
 SECONDS_ELAPSED=$((RUN_TIME - MAY_15_2022))
 DAYS_ELAPSED=$((SECONDS_ELAPSED / (60 * 60 * 24)))
 
+# test case
 # DAYS_ELAPSED=1
 
 # calculate the offset, correcting overflow
 OFFSET=$(( $DAYS_ELAPSED  % 364 ))
-echo "The offset is $OFFSET"
+echo "The offset is $OFFSET days"
 
 # map the offset to the value in the pattern
 PATTERN_VALUE=${PATTERN:OFFSET:1}
 
 # conditionally write to a text file
 if ((PATTERN_VALUE == 0)); then
-   echo "The pattern value is 0"        
+    echo "The pattern value is 0"        
 fi
 
 if ((PATTERN_VALUE == 1)); then
-   echo "The pattern value is 1, so a commit will be made"   
-   printf $"Made a commit at: $(date)\n" >> log.txt     
+    echo "The pattern value is 1, so a commit will be made"   
+    printf $"Made a commit at: $(date)\n" >> log.txt     
 fi
